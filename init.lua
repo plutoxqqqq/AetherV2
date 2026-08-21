16dbc04679a156e363c352bb54087cc1b539a7f4
plutoxqqqq <josephversace111@gmail.com>
2026-08-21 17:45:23 +1000

    Update init.lua

--- init.lua
+++ init.lua
@@ -826,7 +826,7 @@ local function wipeFolder(path)
 end
 
 
-for _, folder in {'aetherv2', 'aetherv2/games', 'aetherv2/profiles', 'aetherv2/assets', 'aetherv2/assets/new', 'aetherv2/libraries', 'aetherv2/guis', 'aetherv2/configs', 'aetherv2/songs', 'aetherv2/songs/spotify'} do
+for _, folder in {'aetherv2', 'aetherv2/games', 'aetherv2/profiles', 'aetherv2/assets', 'aetherv2/assets/new', 'aetherv2/libraries', 'aetherv2/guis', 'aetherv2/configs', 'aetherv2/songs'} do
 	if not isfolder(folder) then
 		_G.AetherV2SetLoadingStatus('Creating '..folder, 0.08)
 		makefolder(folder)
