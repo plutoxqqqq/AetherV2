ba7337ebc47e20bf870f8d4b0ff771e82bd1833d
plutoxqqqq <josephversace111@gmail.com>
2026-09-05 11:13:01 +1000

    Fix WinEffect: set WinEffectType attribute; stop relying on getconnections Function

--- games/6872274481/legit/WinEffect.lua
+++ games/6872274481/legit/WinEffect.lua
@@ -3,20 +3,57 @@ run(function()
     local List
     local NameToId = {}
 
+    local function selectedId()
+        return List and NameToId[List.Value] or nil
+    end
+
+    local function applyAttribute()
+        local id = selectedId()
+        if id ~= nil then
+            lplr:SetAttribute('WinEffectType', id)
+        end
+    end
+
+    local function playEffect()
+        local id = selectedId()
+        if id == nil then return end
+        applyAttribute()
+
+        local remote
+        pcall(function()
+            remote = bedwars.Client:Get('WinEffectTriggered')
+        end)
+        local instance = remote and remote.instance
+        if instance and instance.OnClientEvent then
+            local payload = {
+                winEffectType = id,
+                winningPlayer = lplr
+            }
+            local fired = false
+            pcall(function()
+                for _, conn in getconnections(instance.OnClientEvent) do
+                    local fn = conn.Function or conn.FunctionValue
+                    if type(fn) == 'function' then
+                        fired = true
+                        pcall(fn, payload)
+                    end
+                end
+            end)
+            if not fired then
+                pcall(function()
+                    firesignal(instance.OnClientEvent, payload)
+                end)
+            end
+        end
+    end
+
     WinEffect = vape.Categories.Legit:CreateModule({
         Name = 'WinEffect',
         Function = function(callback)
             if callback then
-                WinEffect:Clean(vapeEvents.MatchEndEvent.Event:Connect(function()
-                    for i, v in getconnections(bedwars.Client:Get('WinEffectTriggered').instance.OnClientEvent) do
-                        if v.Function then
-                            v.Function({
-                                winEffectType = NameToId[List.Value],
-                                winningPlayer = lplr
-                            })
-                        end
-                    end
-                end))
+                applyAttribute()
+                WinEffect:Clean(lplr:GetAttributeChangedSignal('WinEffectType'):Connect(applyAttribute))
+                WinEffect:Clean(vapeEvents.MatchEndEvent.Event:Connect(playEffect))
             end
         end,
         Tooltip = 'Allows you to select any clientside win effect'
@@ -29,6 +66,11 @@ run(function()
     table.sort(WinEffectName)
     List = WinEffect:CreateDropdown({
         Name = 'Effects',
-        List = WinEffectName
+        List = WinEffectName,
+        Function = function()
+            if WinEffect.Enabled then
+                applyAttribute()
+            end
+        end
     })
 end)
