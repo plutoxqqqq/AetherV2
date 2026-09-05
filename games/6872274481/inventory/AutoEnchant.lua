f3a5f5708b8915b360c426dae1f11f95f919e6ed
plutoxqqqq <josephversace111@gmail.com>
2026-09-05 11:37:17 +1000

    Remove new.core GUI wrapper; skip duplicate AutoEnchant registration

--- games/6872274481/inventory/AutoEnchant.lua
+++ games/6872274481/inventory/AutoEnchant.lua
@@ -1,4 +1,7 @@
 run(function()
+	if vape.Modules and vape.Modules.AutoEnchant then
+		return
+	end
 	local category = vape.Categories.Inventory or vape.Categories.Utility or vape.Categories.World
 	if not category or type(category.CreateModule) ~= 'function' then
 		warn('[AetherV2] AutoEnchant could not find a module category')
